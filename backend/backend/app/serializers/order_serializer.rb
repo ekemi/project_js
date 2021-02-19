0dd23007982775e2fbe0c3a59218272bb2e78f37
Yapnounkie <edmundopierre@yahoo.com>
2021-02-19 01:56:21 -0800

    add serializer

---
+++ backend/backend/app/serializers/order_serializer.rb
@@ -0,0 +1,3 @@
+class OrderSerializer < ActiveModel::Serializer
+  attributes :id, :id, :product_name, :seller, :price
+end
--- frontend/src/model.js
+++ frontend/src/model.js
@@ -11,6 +11,24 @@ class Order {
 
  //Fetch the orders
  static getOrders() {
-     console.log('hello')
+     //Make a fetch to return a promise
+     return fetch("http://127.0.0.1:3000/orders",{
+         headers: {
+             "Accept": "application/json",
+             "Content-Type": "application/json"
+
+         }
+     })
+     .then(resp=>{
+         if(resp.ok){
+             //return a promise parsed as JSON
+             return resp.json()
+         }else{
+             return resp.text().then(error=> Promise.reject(error))
+         }
+     })
+     .then(orderList=>{
+         debugger
+     })
  }
 }
