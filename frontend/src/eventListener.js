a30d2c4a320f2d6fa242f54511a2b88c23aeb636
Yapnounkie <edmundopierre@yahoo.com>
2021-02-19 11:28:39 -0800

    add DOM node

--- frontend/src/eventListener.js
+++ frontend/src/eventListener.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
-   Order.getOrders()
+   Customer.getOrders()
     
    
 })
--- frontend/src/model.js
+++ frontend/src/model.js
@@ -1,8 +1,8 @@
-class Order {
+class Customer {
 
     constructor(attr){
         // Info from the keys from the JSON API
-       let  jsonKeys = [id,product_name, seller, price]
+       let  jsonKeys = [ "id", "name"]
        //Create a new order 
        jsonKeys.forEach(value=>this[value]= attr[value])
     }
@@ -12,7 +12,7 @@ class Order {
  //Fetch the orders
  static getOrders() {
      //Make a fetch to return a promise
-     return fetch("http://127.0.0.1:3000/orders",{
+     return fetch("http://127.0.0.1:3000/customers",{
          headers: {
              "Accept": "application/json",
              "Content-Type": "application/json"
@@ -27,8 +27,38 @@ class Order {
              return resp.text().then(error=> Promise.reject(error))
          }
      })
-     .then(orderList=>{
+     .then(customerList=>{
+         //Store the customer in the class
+         this.collectionCustomers = customerList.map(attrs=>new Customer(attrs))
+         this.renderCustomersList = this.collectionCustomers.map(attrs=> attrs.render())
+         this.container().append(...this.renderCustomersList)
+         return this.collectionCustomers
          debugger
      })
+ }
+
+//  <li><a href="#" target="_blank">List</a></li>
+//  <li><a href="#" target="_blank">List</a></li>
+//  <li><a href="#" target="_blank">List</a></li>
+
+static container () {
+   return this.c ||=document.querySelector('#list')
+}
+
+ render () {
+     this.element ||=document.createElement('li');
+     //this.element.class ="classe names"
+     this.nameLink ||= document.createElement('a')
+      //this.nameLink.classList.add(..."fas fa-edit btnedit".split(" "))
+       this.nameLink.textContent= this.name;
+       
+       this.editLink  ||=  document.createElement('a') 
+       //this.nameLink.class ="classe names"
+       this.editLink.innerHTML = `<i class="fas fa-edit btnedit">`
+       //this.deleteLink.class ="classe names"
+       this.deleteLink ||=  document.createElement('a') 
+       this.deleteLink.innerHTML = `<i class="fas fa-trash-alt btndelete"></i>`
+       this.element.append(this.nameLink,this.editLink,this.deleteLink )
+       return this.element;
  }
 }
