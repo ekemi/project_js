f16d568fb46b4078cf8824dcf31238620f8ce706
Yapnounkie <edmundopierre@yahoo.com>
2021-02-18 18:24:52 -0800

    add validation method

--- frontend/src/order.js
+++ frontend/src/order.js
@@ -7,4 +7,34 @@ class Order {
         this.price = price;
 
     }
+
+
+    
+    getOrder () {
+    //input elements
+        const id = document.getElementById('user-id');
+        const proname = document.getElementById('proname');
+        const seller = document.getElementById('seller');
+        const price = document.getElementById('price');
+        //Button  elements
+        const btnCreate = document.getElementById('btn-create');
+        const btnUpdate = document.getElementById('btn-update');
+        const btnDelete = document.getElementById('btn-delete');
+        const btnRead = document.getElementById('btn-read');
+    }
+
+}
+
+//check validation
+const validation = object =>{
+    let flag = false;
+    for(const value in object){
+        if(object[value]!=""&& object.hasOwnProperty(value)){
+            flag = true;
+        }else {
+            flag = false;
+        }
+    }
+    return flag;
+
 }
